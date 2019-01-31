f4b5c68018a02b08b737d0f44ef267a145e7d6f8
Mario de Moraes Barros Neto <mariombn@gmail.com>
2019-01-31 18:25:19 -0200

    Teste de Conexão SSH com php - Sucesso!

--- public/integracao-ssh.php
+++ public/integracao-ssh.php
@@ -4,23 +4,41 @@ require_once '../bootstrap.php';
 
 $view = \Service\ViewServiceFactory::create();
 
+if (!empty($_POST['acao']) && $_POST['acao'] == 'postSsh') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $command  = $_POST['command'];
+
+    $connection = ssh2_connect('192.168.0.165', 22);
+    ssh2_auth_password($connection, $username, $password);
+
+    $stream = ssh2_exec($connection, $command);
+
+    stream_set_blocking($stream, true);
+    $stream_out = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
+    echo stream_get_contents($stream_out);
+
+    exit;
+}
+
 $view->header();
 
 ?>
 
     <h1>Integração SSH</h1>
     <form action="integracao-ssh.php" method="post">
+        <input type="hidden" name="acao" value="postSsh">
         <div class="row">
             <div class="col-md-6">
                 <div class="form-group">
                     <label for="formGroupExampleInput">Usuário</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Usuario SSH">
+                    <input name="username" type="text" class="form-control" id="formGroupExampleInput" placeholder="Usuario SSH">
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="form-group">
                     <label for="formGroupExampleInput">Senha</label>
-                    <input type="password" class="form-control" id="formGroupExampleInput" placeholder="Senha SSH">
+                    <input name="password" type="password" class="form-control" id="formGroupExampleInput" placeholder="Senha SSH">
                 </div>
             </div>
         </div>
@@ -28,18 +46,21 @@ $view->header();
             <div class="col-md-12">
                 <div class="form-group">
                     <label for="formGroupExampleInput">Comando</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Comando">
+                    <input name="command" type="text" class="form-control" id="formGroupExampleInput" placeholder="Comando">
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-4 col">
-                <div class="form-group">
-                    <label for="formGroupExampleInput">Comando</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Comando">
-                </div>
+            <div class="col align-self-end">
+                <input type="submit" class="btn btn-primary" value="Enviar Comando">
             </div>
         </div>
     </form>
+
+    <?php if (!empty($stream)): ?>
+        <div class="row">
+            <?php echo $stream ?>
+        </div>
+    <?php endif ?>
 
 <?php $view->footer() ?>
