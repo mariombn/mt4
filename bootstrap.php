790f97829750789683d2a9f13fca5809cd6939c4
Mario de Moraes Barros Neto <mariombn@gmail.com>
2019-01-31 11:35:34 -0200

    Arquitetura para carregar a view

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,25 @@
 <?php
 
-// Definição das Constantes
+/**
+ * MT4 - Aplicação WeB de Segurança
+ * Autor: Mario de Moraes Barros Neto <mariombn@gmail.com>
+ * Github: https://github.com/mariombn/mt4
+ */
 
+// Definição das Constantes de Diretórios
 define('SYSTEM_PATH', __DIR__);
 define('PUBLIC_SYSTEM_PATH', SYSTEM_PATH . '/public');
+define('CONFIG_SYSTEM_PATH', SYSTEM_PATH . '/config');
 
+// Carregando Configurações da Aplicação
+$configs = parse_ini_file(CONFIG_SYSTEM_PATH . '/config.ini');
+if (is_array($configs)) {
+    foreach ($configs as $k => $v) {
+        define($k, $v);
+    }
+}
+
+// Autoload sem Composer
 spl_autoload_register(function($classe){
     $classe = 'src/' . str_replace('\\', '/', $classe) . '.php';
     require_once $classe;
